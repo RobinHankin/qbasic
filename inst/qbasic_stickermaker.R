b4472a85524a293692a44458dac9dfd7ac483190
Robin Hankin <hankin.robin@gmail.com>
2023-06-08 13:19:15 +1200

    hex sticker

---
+++ inst/qbasic_stickermaker.R
@@ -0,0 +1,4 @@
+library("hexSticker")
+sticker("monster_blue.png", package="qbasic", p_size=24, s_x=1, s_y=0.84,
+s_width=0.7,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
+h_color="#000000", filename="qbasic.png")
