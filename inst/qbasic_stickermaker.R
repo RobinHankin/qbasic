98ca14288c5b8b8236af3ea896de15c405de947b
Robin Hankin <hankin.robin@gmail.com>
2023-06-08 16:04:58 +1200

    comments and changes to stickermaker

--- inst/qbasic_stickermaker.R
+++ inst/qbasic_stickermaker.R
@@ -1,4 +1,10 @@
+## This file makes the hex sticker in file qbasic.png, which lives in
+## the man/figures directory of the package.  The letter 'q' is in the
+## Luminari font, with a modified tick that matches the nearest edge
+## of the hexagon.
+
 library("hexSticker")
-sticker("monster_blue.png", package="qbasic", p_size=24, s_x=1, s_y=0.84,
-s_width=0.7,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
-h_color="#000000", filename="qbasic.png")
+
+sticker("qbasic_modified_tick.png", package="qbasic", p_size=36, s_x=1, s_y=0.91,
+        s_width=0.6,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
+        h_color="#000000", filename="qbasic.png")
