a3997ac418390f7b5b986f5ee725692c397862cb
Robin Hankin <hankin.robin@gmail.com>
2023-06-14 16:21:36 +1200

    working first version for complex multiplication

---
+++ src/queueR.cpp
@@ -0,0 +1,24 @@
+#include <Rcpp.h>
+#include <cmath>
+
+#include "queueR.h"
+
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericVector complex_multiply(const NumericVector a_real, const NumericVector a_imag,
+                               const NumericVector b_real, const NumericVector b_imag) {
+  const size_t n = a_real.size();
+  NumericVector result_real(n);
+  NumericVector result_imag(n);
+  
+  for (int i = 0; i < n; i++) {
+    const std::complex<double> a(a_real[i], a_imag[i]);
+    const std::complex<double> b(b_real[i], b_imag[i]);
+    const std::complex<double> product = a * b;
+    result_real[i] = std::real(product);
+    result_imag[i] = std::imag(product);
+  }
+  
+  return Rcpp::cbind(result_real, result_imag);
+}
---
+++ src/queueR.h
@@ -0,0 +1,11 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+
+
+#define STRICT_R_HEADERS
+#include <Rcpp.h>
+#include <iterator>
+#include <vector>
+#include <assert.h>
+
+using namespace std;
+using namespace Rcpp;
