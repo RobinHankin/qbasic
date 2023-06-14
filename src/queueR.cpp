a64feed29484e1ebe8cf3f331072b7719c0b8948
Robin Hankin <hankin.robin@gmail.com>
2023-06-15 07:56:17 +1200

    was incrementing i twice; also call 'n' 'r' and some tidyup

--- src/queueR.cpp
+++ src/queueR.cpp
@@ -5,27 +5,23 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 NumericVector complex_multiply(const NumericVector a_real, const NumericVector a_imag,
                                const NumericVector q_real, const NumericVector q_imag) {
-  const size_t n = a_real.size();
-  NumericVector result_real(n);
-  NumericVector result_imag(n);
+  const size_t r = a_real.size();
+  NumericVector result_real(r);
+  NumericVector result_imag(r);
   
   const std::complex<double> one (1,0);
 
-  for (int i = 0; i < n; i++) {
-          std::complex<double> out (1,0);
+  for (int i = 0; i < r; i++) {
+          std::complex<double> out = one;
           std::complex<double> outold (0,0);
     const std::complex<double> a (a_real[i], q_imag[i]);
     const std::complex<double> q (q_real[i], q_imag[i]);
           std::complex<double> s (a_real[i], a_imag[i]);  // s == subt
 
-    while(
-	  (std::real(outold) != std::real(out)) ||
-	  (std::imag(outold) != std::imag(out))
-	  ){
+    while((std::real(outold) != std::real(out)) || (std::imag(outold) != std::imag(out))){
       outold = out;
-      out = out*(one-s);
-      s = s * q;
-      i++;
+      out *= (one-s);
+      s *= q;
     }
     result_real[i] = std::real(out);
     result_imag[i] = std::imag(out);
