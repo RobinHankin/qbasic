ad0531ede756d26624f784818408738867b37aca
Robin Hankin <hankin.robin@gmail.com>
2023-06-16 16:05:20 +1200

    consistentification

--- R/queueR.R
+++ R/queueR.R
@@ -1,31 +1,3 @@
-process_args <- function(a,q,n){
-  if(any(n<0)){
-    stopifnot(all(n==round(n)))
-    neg_found <- TRUE
-  } else {
-    neg_found <- FALSE
-  }
-
-  jj <- cbind(a,q,n)
-  a <- jj[,1]
-  q <- jj[,2]
-  n <- Re(jj[,3])
-  if(neg_found){
-    wanted <- which(n<0)
-    n <- abs(n)
-    a[wanted] <- a[wanted]/q[wanted]^n[wanted]
-  }
-  return(cbind(Re(a),Im(a),Re(q),Im(q),n))
-}
-
-process_output <- function(n,z){
-  if(any(n<0)){
-    wanted <- which(n<0)
-    z[wanted] <- 1/z[wanted]
-  }
-  return(z)
-}
-
 poch_complex_n <- function(a,q=a,alpha){
   jj <- cbind(Re(a),Im(a),Re(q),Im(q),Inf)
   jj <- pochhammer(a_real=jj[,1],a_imag=jj[,2], q_real=jj[,3],q_imag=jj[,4], n=jj[,5])
@@ -40,12 +12,31 @@ poch_complex_n <- function(a,q=a,alpha){
 }
 
 `poch` <- function(a,q=a,n=Inf){
-  if(is.complex(n)){return(poch_complex_n(a,q,n))}
-  jj <- process_args(a,q,n)
+  if(any(n != round(n))){return(poch_complex_n(a,q,n))}
+
+  neg_found <- any(n<0)
+  if(neg_found){
+    wanted <- which(n<0)
+    n <- abs(n)
+    a[wanted] <- a[wanted]/q[wanted]^n[wanted]
+  }
+
+  jj <- cbind(Re(a),Im(a),Re(q),Im(q),n)
   jj <- pochhammer(a_real=jj[,1],a_imag=jj[,2], q_real=jj[,3],q_imag=jj[,4], n=jj[,5])
   
-  z <- process_output(n, z=jj[,1] + 1i*jj[,2])
+  z <- jj[,1] + 1i*jj[,2]
+  if(neg_found){ z[wanted] <- 1/z[wanted] }
+
   if((!is.complex(q)) && !is.complex(a)){ z <- Re(z) }
+  
+  if(length(a) >= length(q)){
+    attr <- attributes(a)
+  } else {
+    attr <- attributes(q)
+  }
+
+  if((length(a) == length(q)) && (length(a)==1) && (length(n)>1)){ attr <- attributes(n) }
+  attributes(z) <- attr
   return(z)
 }
 
