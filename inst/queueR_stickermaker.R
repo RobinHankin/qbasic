6b5ae8254f200fbfed864c1334221886a90656dc
Robin Hankin <hankin.robin@gmail.com>
2023-06-21 15:15:55 +1200

    change size of package name

--- inst/queueR_stickermaker.R
+++ inst/queueR_stickermaker.R
@@ -5,6 +5,6 @@
 
 library("hexSticker")
 
-sticker("queueR_modified_tick.png", package="queueR", p_size=36, s_x=1, s_y=0.91,
+sticker("queueR_modified_tick.png", package="queueR", p_size=30, s_x=1, s_y=0.91,
         s_width=0.6,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
         h_color="#000000", filename="queueR.png")
